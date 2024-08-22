b7bda9a3edfe58761a62761e94df79a2d42c490d
Lee-d0 <marmar11137@gmail.com>
2024-08-22 11:33:03 -0400

    Trailer page done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,6 +7,8 @@ import { Toaster } from "react-hot-toast"
 import { useAuthStore } from "./store/authUser.js"
 import { useEffect } from "react"
 import { Loader } from "lucide-react"
+import WatchPage from "./Pages/WatchPage.jsx"
+
 
 
 
@@ -36,6 +38,8 @@ function App() {
       <Route path="/" element={<HomePage/>} />
       <Route path="/login" element={!user ? <LoginPage/> : <Navigate to={"/"}/>} />
       <Route path="/signup" element={!user ? <SignUpPage/> : <Navigate to={"/"}/>} />
+      <Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
+      
     </Routes>
     <Footer />
 
