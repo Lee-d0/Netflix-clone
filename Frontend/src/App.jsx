b709629f5bec8bd90d4a29b8567a9c02d099a2be
Lee-d0 <marmar11137@gmail.com>
2024-08-25 20:26:58 -0400

    Searching and Search History added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,6 +8,8 @@ import { useAuthStore } from "./store/authUser.js"
 import { useEffect } from "react"
 import { Loader } from "lucide-react"
 import WatchPage from "./Pages/WatchPage.jsx"
+import SearchPage from "./Pages/SearchPage.jsx"
+import SearchHistoryPage from "./Pages/SearchHistoryPage.jsx"
 
 
 
@@ -39,6 +41,8 @@ function App() {
       <Route path="/login" element={!user ? <LoginPage/> : <Navigate to={"/"}/>} />
       <Route path="/signup" element={!user ? <SignUpPage/> : <Navigate to={"/"}/>} />
       <Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
+      <Route path='/search' element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
+      <Route path='/history' element={user ? <SearchHistoryPage /> : <Navigate to={"/login"} />} />
       
     </Routes>
     <Footer />
